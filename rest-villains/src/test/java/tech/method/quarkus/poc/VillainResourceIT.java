52dfd719d2f3435ddb7558254087d4c146a2b5e5
Bruno Araujo <brnasc.work@proton.me>
2024-07-28 17:10:39 -0300

    add base project

---
+++ rest-villains/src/test/java/tech/method/quarkus/poc/VillainResourceIT.java
@@ -0,0 +1,8 @@
+package tech.method.quarkus.poc;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class VillainResourceIT extends VillainResourceTest {
+    // Execute the same tests but in packaged mode.
+}
